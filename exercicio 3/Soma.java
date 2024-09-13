84da555698c95830c2f22bfc97b2978c33a22100
sknnys <hudrsonsilvahs@hotmail.com>
2024-09-13 10:38:26 -0300

    corrigido erros de indice.

--- exercicio 3/Soma.java
+++ exercicio 3/Soma.java
@@ -1,8 +1,8 @@
 public class Soma {
     public static void main(String[] args) {
-    int INDICE = 13;
+    int INDICE = 12;
     int SOMA = 0;
-    int K = 0;
+    int K = 1;
         while(K < INDICE) {
             K++;
             SOMA = SOMA + K;
